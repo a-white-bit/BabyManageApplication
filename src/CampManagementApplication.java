3b7d7384a88ad5546f78bfd5ef6600c1fbd3f840
guchi2024 <kjo0961@naver.com>
2024-05-03 21:00:39 +0900

    수강생 등록 1)

--- src/CampManagementApplication.java
+++ src/CampManagementApplication.java
@@ -2,9 +2,7 @@ import model.Score;
 import model.Student;
 import model.Subject;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Notification
@@ -163,16 +161,26 @@ public class CampManagementApplication {
             }
         }
     }
-
+// HashMap
+private static Map<String, Student> studentMap = new HashMap<>();
     // 수강생 등록
     private static void createStudent() {
+
         System.out.println("\n수강생을 등록합니다...");
+
+        System.out.print("수강생 ID: ");
+        String studentId = sc.next();
+
         System.out.print("수강생 이름 입력: ");
         String studentName = sc.next();
+        System.out.print("수강생 필수 과목: ");
+        String studentSubject = sc.next();
         // 기능 구현 (필수 과목, 선택 과목)
 
-        Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName); // 수강생 인스턴스 생성 예시 코드
+
+        Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName, studentSubject); // 수강생 인스턴스 생성 예시 코드
         // 기능 구현
+        studentMap.put(studentId, student);
         System.out.println("수강생 등록 성공!\n");
     }
 
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,12 +1,23 @@
 package model;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Student {
+    // HashMap 추가
+    Map<String, String> strMap = new HashMap<>();
+
     private String studentId;
     private String studentName;
+    //학생 과목 추가
+    private String studentSubject;
 
-    public Student(String seq, String studentName) {
+    public Student(String seq, String studentName, String studentSubject) {
         this.studentId = seq;
         this.studentName = studentName;
+        this.studentSubject = studentSubject;
+
+        this.strMap.put(studentId, studentName);
     }
 
     // Getter
@@ -18,4 +29,7 @@ public class Student {
         return studentName;
     }
 
+    public String getStudentSubject() {
+        return studentSubject;
+    }
 }
