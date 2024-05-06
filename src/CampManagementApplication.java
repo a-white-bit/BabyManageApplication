455c8d1c615662830c2ee36205ca3dd799afcbc9
Wooseok1213 <162704734+Wooseok1213@users.noreply.github.com>
2024-05-06 13:14:01 +0900

    조회 Scanner 추가

--- src/CampManagementApplication.java
+++ src/CampManagementApplication.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/** push test 용입니다.
+/** 수강생 상태별 목록을 조회하는 코드를 구현해보자
  * Notification
  * Java, 객체지향이 아직 익숙하지 않은 분들은 위한 소스코드 틀입니다.
  * main 메서드를 실행하면 프로그램이 실행됩니다.
@@ -43,7 +43,9 @@ public class CampManagementApplication {
             System.out.println("\n오류 발생!\n프로그램을 종료합니다.");
         }
     }
-
+    private studentState(){
+        studentStore = new ArrayList<>();
+    }
     // 초기 데이터 생성
     private static void setInitData() {
         studentStore = new ArrayList<>();
@@ -146,14 +148,16 @@ public class CampManagementApplication {
             System.out.println("수강생 관리 실행 중...");
             System.out.println("1. 수강생 등록");
             System.out.println("2. 수강생 목록 조회");
-            System.out.println("3. 메인 화면 이동");
+            System.out.println("3. 상태별 수강생 목록 조회");
+            System.out.println("4. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요...");
             int input = sc.nextInt();
 
             switch (input) {
                 case 1 -> createStudent(); // 수강생 등록
                 case 2 -> inquireStudent(); // 수강생 목록 조회
-                case 3 -> flag = false; // 메인 화면 이동
+                case 3 -> studentStateInquire(); // 수강생 목록 조회
+                case 4 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
                     flag = false;
@@ -167,17 +171,32 @@ public class CampManagementApplication {
         System.out.println("\n수강생을 등록합니다...");
         System.out.print("수강생 이름 입력: ");
         String studentName = sc.next();
-        // 기능 구현 (필수 과목, 선택 과목)
+        System.out.print("수강생 상태 입력 (1 : 좋음  2 : 보통  3 : 나쁨 ) : ");
+        String studentState = sc.next();
 
-        Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName); // 수강생 인스턴스 생성 예시 코드
-        // 기능 구현
+        // 기능 구현 (필수 과목, 선택 과목)
+        // 수강생 인스턴스 생성 예시 코드
+        Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName, studentState);
         System.out.println("수강생 등록 성공!\n");
     }
 
     // 수강생 목록 조회
     private static void inquireStudent() {
         System.out.println("\n수강생 목록을 조회합니다...");
-        // 기능 구현
+        System.out.println(getStudentId());
+        System.out.println("\n수강생 목록 조회 성공!");
+    }
+
+    // 상태별 수강생 목록 조회
+    private static void studentStateInquire() {
+        System.out.print("\n"+"조회할 학생들의 상태를 입력해주세요 (1 : 좋음  2 : 보통  3 : 나쁨  :  )");
+        String stateNumber = sc.next();
+        switch (stateNumber) {
+//            case 1 -> {
+//                System.out.print(createStudent(std));
+//
+//            }
+        }
         System.out.println("\n수강생 목록 조회 성공!");
     }
 
@@ -210,6 +229,10 @@ public class CampManagementApplication {
         System.out.print("\n관리할 수강생의 번호를 입력하시오...");
         return sc.next();
     }
+    private static String getStudentState() {
+        System.out.print("\n관리할 수강생의 상태를 번호로 입력하시오 (1 : 좋음  2 : 보통  3 : 나쁨  :  )"); //관리할 학생의 기분?
+        return sc.next();
+    }
 
     // 수강생의 과목별 시험 회차 및 점수 등록
     private static void createScore() {
@@ -236,5 +259,14 @@ public class CampManagementApplication {
         // 기능 구현
         System.out.println("\n등급 조회 성공!");
     }
+
+    private static void inquireStudentState() {
+        String studentId = getStudentId(); // 관리할 수강생 고유 번호
+        // 기능 구현 (조회할 특정 과목)
+        System.out.println("상태별 학생의 목록을 조회합니다...");
+        // 기능 구현
+        System.out.println("\n등급 조회 성공!");
+    }
+
 
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -3,19 +3,27 @@ package model;
 public class Student {
     private String studentId;
     private String studentName;
+    private String studentState;
 
-    public Student(String seq, String studentName) {
+    public Student(String seq, String studentName, String studentCondition) {
         this.studentId = seq;
         this.studentName = studentName;
+        this.studentState = studentState;
     }
-
     // Getter
     public String getStudentId() {
         return studentId;
     }
-
     public String getStudentName() {
         return studentName;
     }
-
+    public String getStudentState() {
+        return studentState;
+    }
+//    public String studentState() {
+//        if (studentState.equals("1")){
+//    } else if (studentState.equals("2")){
+//        } else if (studentState.equals("3")){
+//        }; return studentState;
+//    }
 }
