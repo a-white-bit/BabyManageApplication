5d800953b09100b5e78ed324503a0abc02e409b9
soyoung <soyoung.yang94@gmail.com>
2024-05-07 12:00:38 +0900

    학생 상태별 조회 기능 수정: 해당사항이 아닐 경우 안내 문구 출력

--- src/CampManagementApplication.java
+++ src/CampManagementApplication.java
@@ -6,10 +6,16 @@ import java.util.*;
 
 /**
  * 구현 메모
- * <p>
+ * -
  * 1. 숫자가 아닌 값 입력되면 오류로 프로그램 종료됨
  * 프로그램은 종료되지 않게 하고, 다시 값을 받게 하거나 수정단계만 빠져나가도록 하기
  * switch 변수를 String 형으로 작성  or 예외 try-catch
+ * -
+ * 2. 학생이 수강하는 과목리스트 등록
+ * 중복 x, 최대 선택 개수 지정
+ * -
+ * 3. 학생이 수강하는 과목리스트 출력
+ * 필수과목 - 선택과목 분류해서 예쁘게 출력하기
  */
 
 public class CampManagementApplication {
@@ -151,7 +157,7 @@ public class CampManagementApplication {
         boolean state = true;
         String studentState = "";
         while (state) {
-            System.out.print("(1.아주좋음, 2.좋음, 3.보통, 4.나쁨, 5.아주나쁨)\n수강생 상태 입력: ");
+            System.out.print("수강생 상태 입력:\n(1.아주좋음, 2.좋음, 3.보통, 4.나쁨, 5.아주나쁨) ");
             studentState = sc.next();
             switch (studentState) {
                 case "1":
@@ -188,7 +194,7 @@ public class CampManagementApplication {
         studentSubject.add("MySQL");
         boolean flag = true;
         while (flag) {
-            System.out.print("(1.디자인 패턴, 2.Spring security, 3.Redis, 4.MongoDB, 5.종료)\n수강할 선택 과목: ");
+            System.out.print("수강할 선택 과목:\n(1.디자인 패턴, 2.Spring security, 3.Redis, 4.MongoDB, 5.종료) ");
             switch (sc.next()) {
                 case "1":
                     studentSubject.add("디자인 패턴");
@@ -208,16 +214,16 @@ public class CampManagementApplication {
                 default:
                     System.out.println("올바른 값을 입력해주세요.");
             }
-
-            // 수강생 ID 시퀀스 생성
-            String studentId = sequence(INDEX_TYPE_STUDENT);
-
-            // 수강생 인스턴스 생성 예시 코드
-            Student student = new Student(studentId, studentName, studentState, studentSubject);
-            // 학생 목록(Map)에 저장
-            studentStore.put(studentId, student);
-            System.out.println("수강생 등록 성공!\n");
         }
+
+        // 수강생 ID 시퀀스 생성
+        String studentId = sequence(INDEX_TYPE_STUDENT);
+
+        // 수강생 인스턴스 생성 예시 코드
+        Student student = new Student(studentId, studentName, studentState, studentSubject);
+        // 학생 목록(Map)에 저장
+        studentStore.put(studentId, student);
+        System.out.println("수강생 등록 성공!\n");
     }
 
     private static void displayStudentListView() throws InterruptedException {
@@ -291,10 +297,16 @@ public class CampManagementApplication {
             System.out.println("상태 : " + student.getStudentState());
 
             Set<String> subjects = student.getStudentSubject();
+
+            /*
+            * 학생이 수강하는 과목리스트 출력
+            * 필수과목 / 선택과목 분류해서 예쁘게 출력 예정
+             */
             System.out.println("과목 리스트 : ");
             for (String subject : subjects) {
                 System.out.print("{" + subject + "} ");
             }
+
         } else { // 조회한 수강생이 없을 경우
             System.out.println("등록되지 않은 수강생입니다");
             return;
@@ -304,24 +316,24 @@ public class CampManagementApplication {
     }
 
     // 수강생 상태별 목록 조회
-    /*
-     * 조회하고 싶은 상태를 사용자에게 입력받습니다. (예시: "아주좋음")
-     * 수강생 목록은 studentStore에 있고, Map 컬렉션을 사용합니다. Entry set문법으로 Map의 키, Value 를 받아옴
-     * Map의 모든 요소들을 돌면서  통일성있게 swich 문으로 입력받은 상태 "아주좋음"과 일치하는 요소들의
-     *  ID와 이름으로 출력해주세요.
-     * 아래 참고하시면 좋을 것 같습니다!
-     * https://velog.io/@woply/HashMap-%EC%A0%84%EC%B2%B4-%EA%B0%92%EC%9D%84-%EC%B6%9C%EB%A0%A5%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
-     * https://coding-factory.tistory.com/556
-     */
-
-    /*기능구현
-     * 학생들의 상태값을 스캐너로 입력받아 swich ~ case 문으로 상태별 조건을 할당해준다.
-     * Map의 값을 가져오기위해 Entry 문을 사용하여 studentStore 의 값을 entryset 으로 할당해준 후
-     * for문으로 Map 을 전체적으로 돌면서 사용자가 입력한 key를 입력받아 학생 ID와 이름을 value 값으로 넘겨준다
-     * 출력단에서 studentStore 에서 직접 뽑아오는게 아닌 if 문에서의 조건에 해당하는 밸류값을
-     * studentStore 를 할당받은 entryset 에서 출력해준다.
-     */
     private static void inquireStudentByState() {
+        /*
+         * 조회하고 싶은 상태를 사용자에게 입력받습니다. (예시: "아주좋음")
+         * 수강생 목록은 studentStore에 있고, Map 컬렉션을 사용합니다. Entry set문법으로 Map의 키, Value 를 받아옴
+         * Map의 모든 요소들을 돌면서  통일성있게 swich 문으로 입력받은 상태 "아주좋음"과 일치하는 요소들의
+         *  ID와 이름으로 출력해주세요.
+         * 아래 참고하시면 좋을 것 같습니다!
+         * https://velog.io/@woply/HashMap-%EC%A0%84%EC%B2%B4-%EA%B0%92%EC%9D%84-%EC%B6%9C%EB%A0%A5%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95
+         * https://coding-factory.tistory.com/556
+         */
+
+        /*기능구현
+         * 학생들의 상태값을 스캐너로 입력받아 swich ~ case 문으로 상태별 조건을 할당해준다.
+         * Map의 값을 가져오기위해 Entry 문을 사용하여 studentStore 의 값을 entryset 으로 할당해준 후
+         * for문으로 Map 을 전체적으로 돌면서 사용자가 입력한 key를 입력받아 학생 ID와 이름을 value 값으로 넘겨준다
+         * 출력단에서 studentStore 에서 직접 뽑아오는게 아닌 if 문에서의 조건에 해당하는 밸류값을
+         * studentStore 를 할당받은 entryset 에서 출력해준다.
+         */
         System.out.println("\n수강생을 상태별로 조회합니다...");
         System.out.print("조회할 학생들의 상태를 입력해주세요 \n(1.아주좋음, 2.좋음, 3.보통, 4.나쁨, 5.아주나쁨)\n수강생 상태 입력 : ");
         String studentState = sc.next();
@@ -336,14 +348,25 @@ public class CampManagementApplication {
                 return;
             }
         }
+
+        Map<String, String> studentSortByState = new HashMap<>();
         for (Map.Entry<String, Student> entryset : studentStore.entrySet()) {
             if (studentState.equals(entryset.getValue().getStudentState())) {
-                System.out.println("수강생 ID : " + entryset.getValue().getStudentId() + ", 해당 수강생들 이름 : " + entryset.getValue().getStudentName());
+                studentSortByState.put(entryset.getValue().getStudentId(), entryset.getValue().getStudentName());
             }
         }
-        // 수강생 정보 수정
+
+        System.out.println("<상태: " + studentState + ">");
+        if (studentSortByState.isEmpty()) {
+            System.out.println("해당하는 학생이 없습니다.");
+        } else {
+            studentSortByState.forEach((studentId, studentName) -> {
+                System.out.println("ID: " + studentId + ", 이름: " + studentName);
+            });
+        }
     }
 
+    // 수강생 정보 수정
     private static void updateStudent() {
         System.out.println("\n수강생 정보를 수정합니다...");
         System.out.println("\n수강생 고유번호를 입력해주세요: ");
@@ -413,23 +436,6 @@ public class CampManagementApplication {
         System.out.println("\n수강생 정보를 삭제합니다...");
         System.out.println("\n수강생 고유번호를 입력해주세요: ");
         String studentId = sc.next();
-        /* 컬렉션이 list 일 때 코드
-        Iterator<Student> it = studentStore.iterator();
-        while (it.hasNext()) {
-            Student studentToDelete = (Student)it.next();
-            if (studentToDelete.getStudentId().equals(studentId)) {
-                System.out.print("정말로 " + studentToDelete.getStudentName() + "님의 정보를 삭제하시겠습니까?:\n ('네' 입력시 삭제) ");
-                if ("네".equals(sc.next())) {
-                    it.remove();
-                    System.out.println("삭제되었습니다.");
-                } else {
-                    System.out.println("\n삭제가 취소되었습니다.");
-                }
-                return;
-            }
-        }
-        System.out.println("해당 수강생이 없습니다.");
-        */
         Student deleteStudent = studentStore.get(studentId);
         if (deleteStudent != null) {
             System.out.print("정말로 " + deleteStudent.getStudentName() + "님의 정보를 삭제하시겠습니까?:\n ('네' 입력시 삭제) ");
